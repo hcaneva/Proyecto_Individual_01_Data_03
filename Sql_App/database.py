2f54b26daf51cc1c1e05f8c46fc1b315f2b9294f
hcaneva <105667278+hcaneva@users.noreply.github.com>
2022-09-13 20:59:39 -0300

    Update database.py

--- Sql_App/database.py
+++ Sql_App/database.py
@@ -2,4 +2,4 @@
 from sqlalchemy import create_engine
 
 # Utilizaremos la función create_engine() para encender el motor sql que comunicará nuestras consultas a la base de datos
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/PI01")
+engine = create_engine("mysql+pymysql://TU_USUARIO:TU_CONTRASEÑA@localhost:TU_PUERTO/TU_BASE_DE_DATOS")
